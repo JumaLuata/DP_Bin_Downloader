f9ff970261f28afe082768d05ab78d2efc5c5e55
Xudong Yao <xudong.yao@nxp.com>
2018-04-09 12:42:11 +0800

    V1.0

--- DP_Bin_Downloader.py
+++ DP_Bin_Downloader.py
@@ -2,18 +2,18 @@
 import os
 import sys
 import json
-import re
 import urllib
 import time
 from requests import Session
 from bs4 import BeautifulSoup
 
-#downloader binary
-#urllib.urlretrieve("url","binary/ide @ target @ testcase .out")
+
 class dapeng:
     def __init__(self,date):
-        self.re_url=date
-        self.page='all'
+        self.re_url = date[0]
+        self.save_path = 'binary/'+date[1]+'/'
+        if os.path.exists(self.save_path) is False:
+            os.makedirs(self.save_path)
         self.headers={
             "Host": "10.192.225.198",
             "Cookie": "csrftoken=2FKWEGBZ3QWCEGduuElOzT5kIcZtAvVx",
@@ -24,11 +24,12 @@ class dapeng:
     def start(self):
         self.s = Session()  # 定义一个Session()对象
         dapeng.__get_bin(self, self.re_url)
-        print 'Successful Download.'
+        cmd_shell ('Successful Download.')
+        cmd_shell ('The binaries are saved at '+ self.save_path)
         os._exit(0)
 
     def __get_bin(self,r_url):
-        print r_url
+        cmd_shell(r_url)
         _json = dict()
         _request = []
         case_num = 0
@@ -42,40 +43,57 @@ class dapeng:
             _json[name[3]] = str(tr.find('td', id="td_list_mcuauto_target").get_text())
             _request.append(eval(str(_json).replace("\\n","").replace("\\t","")))
             case_num += 1
-
+        #Get the download address
         in_json = json.dumps(_request)
         req = json.loads(in_json)
-        print "There are " + str(case_num) + " to download.\n"
+        cmd_shell ( "\nThere are *" + str(case_num) + "* binaries to download.\n")
+        cmd_shell ('The binaries are saved at ' + self.save_path)
 
+        #Download
         for i in range(case_num):
             case_url = 'http://10.192.225.198/dapeng/information/requestdetail/'+str(req[i].get('id'))
-            html_case = self.s.get(case_url, headers=self.headers).text
+            html_case = ''
+            try:
+                html_case = self.s.get(case_url, headers=self.headers).text
+            except:
+                print 'Website access failed!'
+                os._exit(1)
             soup_case = BeautifulSoup(html_case, "lxml")
             table = soup_case.find('div', class_="second-div-custom").find_all('table')
             td = table[2].find_all('td', class_='td-custom')
             case_url = 'http://10.192.225.198' + str(td[3].a['href'])
-            case_name = 'binary/'+str(req[i].get('compiler'))+'@'+str(req[i].get('target'))+'@'+str(req[i].get('test_case'))+'.out'
-            urllib.urlretrieve(case_url, case_name)
-            print str(i)+' : [ '+case_name+' ] has been downloaded.'
-            time.sleep(1)
+            case_name = str(req[i].get('compiler'))+'@'+str(req[i].get('target'))+'@'+str(req[i].get('test_case'))+'.out'
+            urllib.urlretrieve(case_url, (self.save_path+case_name))
+            cmd_shell (str(i)+' : [ '+case_name+' ] has been downloaded.')
+            time.sleep(5)#延迟5S等文件下载完
 
+def cmd_shell(str=''):
+    sys.stdout.write(str+'\n')
+    sys.stdout.flush()
 
 if __name__ == '__main__':
-
+    DEBUG = 0
     argvs = sys.argv
-    DEBUG = 1
     request_url = ''
+    request_id = ''
 
     if DEBUG == 1:
         request_url = "http://10.192.225.198/dapeng/information/request/13819"
+        request_id = '13819'
     else:
         if len(argvs) == 2:
             request_url = "http://10.192.225.198/dapeng/information/request/" + str(argvs[1])
-            print "The request url is "+ str(request_url)
+            request_id = str(argvs[1])
+            #cmd_shell("The request url is "+ request_url)
+            cmd_shell('Download All?(y/n):')
+            sw_all = sys.stdin.read(1)
+            if sw_all == 'y':
+                request_url += '/?page=all'
         else:
             print "\n* ERROR!"
             print "\n* Please input a useful Dapeng request id."
             print "\n* Like: python DP_Bin_Downloader.py <request_id>"
             os._exit(1)
 
-    dapeng(request_url).start()
+    request = [request_url ,request_id]
+    dapeng(request).start()
--- readme.md
+++ readme.md
@@ -1,9 +1,26 @@
-# The Dapeng Binary Downloader #
+The Dapeng Binary Downloader
+======================
+![Alt text](http://p6wf2jj0b.bkt.clouddn.com/dapeng_log.jpg)
 
-## Need to install  ##
-pip install beautifulsoup4
+First you need to install
+----------------------
 
-pip install lxml
+> pip install beautifulsoup4
 
+> pip install lxml
 
+How to use the script
+----------------------
+
+1. Open cmd in the script path.
+2. use the command:
+> python DP_Bin_Downloader.py <request_id>
+
+The <request_id> is the part of the red box in the picture.
+
+![Alt text](http://p6wf2jj0b.bkt.clouddn.com/request_id.PNG)
+
+3. Then the script will ask you if download all binaries.You need to input 'y' or 'n'.
+If you input 'n', the script will only download one page (up to 20) binaries.
+4. The out files are saved at <script_path>/binary/<request_id>.
 
